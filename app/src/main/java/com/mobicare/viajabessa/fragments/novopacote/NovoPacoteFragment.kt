f98f85ea8cbc621d53a0d5919d1b27574775bcc9
Marcelo Vergara <marcelorv@gmail.com>
2021-03-25 20:37:42 -0300

    view model com firestore ok

--- app/src/main/java/com/mobicare/viajabessa/fragments/novopacote/NovoPacoteFragment.kt
+++ app/src/main/java/com/mobicare/viajabessa/fragments/novopacote/NovoPacoteFragment.kt
@@ -1,10 +1,12 @@
 package com.mobicare.viajabessa.fragments.novopacote
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -13,6 +15,8 @@ import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.mobicare.viajabessa.R
 import com.mobicare.viajabessa.databinding.FragmentNovoPacoteBinding
 
@@ -26,6 +30,12 @@ class NovoPacoteFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var viewModel: NovoPacoteViewModel
+
+    //para a imagem
+    private val PICK_IMAGE_REQUEST = 71
+    private var filePath: Uri? = null
+    private var firebaseStore: FirebaseStorage? = null
+    private var storageReference: StorageReference? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,6 +64,25 @@ class NovoPacoteFragment : Fragment() {
         })
 
         binding.btnLogout.setOnClickListener { viewModel.logoutFunc() }
+
+        //inserir dados / binding para event handling
+        binding.btnInserir.setOnClickListener { viewModel.inserirDados(
+            binding.edtTitulo.text.toString(),
+            binding.edtDescricao.text.toString(),
+            binding.edtValor.text.toString(),
+            binding.edtImagem.text.toString()
+        )}
+
+        viewModel.resultadoInsert.observe(viewLifecycleOwner, Observer { result ->
+            if(result){
+                binding.edtTitulo.setText("")
+                binding.edtDescricao.setText("")
+                binding.edtValor.setText("")
+                binding.edtImagem.setText("")
+                Toast.makeText(requireContext(),"Informações inseridas com sucesso",Toast.LENGTH_SHORT).show()
+
+            }
+        })
 
         return binding.root
     }
--- app/src/main/java/com/mobicare/viajabessa/fragments/novopacote/NovoPacoteViewModel.kt
+++ app/src/main/java/com/mobicare/viajabessa/fragments/novopacote/NovoPacoteViewModel.kt
@@ -1,26 +1,19 @@
 package com.mobicare.viajabessa.fragments.novopacote
 
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.mobicare.viajabessa.R
 
 class NovoPacoteViewModel : ViewModel() {
 
-    //título do pacote
-    var titulo = ""
 
-    //descrição
-    var descricao = ""
-
-    //valor
-    var valor = 0.0
-
-    //imagem do pacote
-    var imagem = ""
 
     //logout + navegação
     private val _logout = MutableLiveData<Boolean>()
@@ -36,5 +29,37 @@ class NovoPacoteViewModel : ViewModel() {
     fun onLogoutCompleto(){
         _logout.value = false
     }
+
+    //inserção de dados no firestore
+    private val _resultadoInsert = MutableLiveData<Boolean>()
+    val resultadoInsert : LiveData<Boolean>
+        get() = _resultadoInsert
+
+    init {
+        _resultadoInsert.value = false
+    }
+
+    fun inserirDados(tit: String, desc: String, din: String, img: String){
+        Log.i("teste", tit)
+        val db = Firebase.firestore
+        val pacote = hashMapOf(
+            "titulo" to tit,
+            "descricao" to desc,
+            "valor" to din,
+            "imagem" to img
+        )
+        db.collection("pacotes")
+            .add(pacote)
+            .addOnSuccessListener { documentRef ->
+                Log.i("teste", documentRef.id)
+                _resultadoInsert.value = true
+
+            }
+            .addOnFailureListener { e ->
+                Log.i("teste", e.toString())
+            }
+    }
+
+
 
 }
