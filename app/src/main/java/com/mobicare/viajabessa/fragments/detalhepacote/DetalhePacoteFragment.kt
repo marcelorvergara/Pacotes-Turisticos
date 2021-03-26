2eb1dffe8ad9183eb0963401a01c0507a6bf2e18
Marcelo Vergara <marcelorv@gmail.com>
2021-03-26 14:38:06 -0300

    fragmento de detalhed do pacote ok

--- app/src/main/java/com/mobicare/viajabessa/fragments/detalhepacote/DetalhePacoteFragment.kt
+++ app/src/main/java/com/mobicare/viajabessa/fragments/detalhepacote/DetalhePacoteFragment.kt
@@ -6,7 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.mobicare.viajabessa.R
+import com.mobicare.viajabessa.databinding.FragmentDetalhePacoteBinding
+import com.mobicare.viajabessa.databinding.FragmentHomeBinding
+import com.squareup.picasso.Picasso
 
 
 /**
@@ -16,6 +21,7 @@ import com.mobicare.viajabessa.R
  */
 class DetalhePacoteFragment : Fragment() {
 
+    private lateinit var viewModel: DetalhePacoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,10 +33,26 @@ class DetalhePacoteFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        val binding =  DataBindingUtil.inflate<FragmentDetalhePacoteBinding>(inflater, R.layout.fragment_detalhe_pacote,container,false)
+
+        //inicializar o view model
+        viewModel = ViewModelProvider(this).get(DetalhePacoteViewModel::class.java)
+
+        binding.detalhePacoteViewModel = viewModel
+
         val args = DetalhePacoteFragmentArgs.fromBundle(requireArguments())
-        Toast.makeText(context,"Título do Pacote: ${args.pacote.titulo}",Toast.LENGTH_SHORT).show()
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detalhe_pacote, container, false)
+
+        viewModel.titulo = args.pacote.titulo
+        viewModel.valor = args.pacote.valor
+        viewModel.descricao = args.pacote.descricao
+
+        Picasso.get().load(args.pacote.imageUrl).into(binding.imgPktDetalhe)
+
+        //Toast.makeText(context,"Título do Pacote: ${args.pacote.titulo}",Toast.LENGTH_SHORT).show()
+
+        return binding.root
+
+
     }
 
 
---
+++ app/src/main/java/com/mobicare/viajabessa/fragments/detalhepacote/DetalhePacoteViewModel.kt
@@ -0,0 +1,15 @@
+package com.mobicare.viajabessa.fragments.detalhepacote
+
+import androidx.lifecycle.ViewModel
+
+class DetalhePacoteViewModel:ViewModel() {
+
+    lateinit var titulo: String
+
+    lateinit var valor: String
+
+    lateinit var descricao: String
+
+    lateinit var imagemUri: String
+
+}
